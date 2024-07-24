193497e22c38765c4d7da7b5e39fc680a249afef
NagaLeelaHari <nagaleelag.cse@srit.ac.in>
2024-07-24 16:04:12 +0530

    Added fcfs sjf files and its ouputs

---
+++ EXP9/SFJ.java
@@ -0,0 +1,59 @@
+package operating;
+import java.util.Scanner;
+
+public class SJF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int p[] = new int[20];
+        int bt[] = new int[20];
+        int wt[] = new int[20];
+        int tat[] = new int[20];
+        int n, temp;
+        float wtavg, tatavg;
+
+        System.out.println("Enter the number of processes:");
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            p[i] = i;
+            System.out.println("Enter Burst Time for Process " + i + ":");
+            bt[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int k = i + 1; k < n; k++) {
+                if (bt[i] > bt[k]) {
+                    temp = bt[i];
+                    bt[i] = bt[k];
+                    bt[k] = temp;
+                    temp = p[i];
+                    p[i] = p[k];
+                    p[k] = temp;
+                }
+            }
+        }
+
+        wt[0] = 0;
+        wtavg = 0;
+        tat[0] = bt[0];
+        tatavg = bt[0];
+
+        for (int i = 1; i < n; i++) {
+            wt[i] = wt[i - 1] + bt[i - 1];
+            tat[i] = tat[i - 1] + bt[i];
+            wtavg += wt[i];
+            tatavg += tat[i];
+        }
+
+        System.out.println("\tPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME");
+        for (int i = 0; i < n; i++) {
+            System.out.println("\tP" + p[i] + "\t\t" + bt[i] + "\t\t" + wt[i] + "\t\t" + tat[i]);
+        }
+
+        System.out.println("Average Waiting Time: " + (wtavg / n));
+        System.out.println("Average Turnaround Time: " + (tatavg / n));
+       
+        sc.close();
+    }
+}
